09b1fd37476b0f741667a5f87f453f9aa98bf45b
Jolieke <60745364+Jolieke@users.noreply.github.com>
2020-04-17 14:16:08 +0200

    v7 (niet afgekregen helaas)

--- opdracht3/js/script2.js
+++ opdracht3/js/script2.js
@@ -3,7 +3,7 @@
 /*eslint 'no-console':0*/
 
 //--------  GEGEVENS AANVRAGEN ---------//
-//var data = 0;
+var data = 0;
 
 var request = new XMLHttpRequest();
 request.withCredentials = true;
@@ -12,13 +12,7 @@ request.withCredentials = true;
 request.open("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php");
 request.responseType = 'json'; //vertelt wat voor soort file het is: json
 request.setRequestHeader("x-rapidapi-key", "800cfaec57mshb3d2fa49ac20cf9p1f6b51jsn3fcd61f320c0"); //sleutelcode die nodig is om de api te gebruiken 
-request.send(); //verzoek verzenden
-
-//--------  GEGEVENS AANMAKEN ---------//
-request.onload = function () {
-    fromOptions(request.response);
-    infoSelected(request.response.countries_stat);
-};
+request.send(data); //verzoek verzenden
 
 //--------  VARIABELEN ---------//
 var i;
@@ -29,52 +23,54 @@ var selectCountry = document.querySelector('.selected'); //variabele die class s
 
 var countryInfo = document.querySelector('article'); //variabele die article uit html selecteert
 
-function fromOptions(countries_stat) {
-    console.log(listOptions);
-    //loop voor het toevoegen van <option> elementen aan <select> in form html 
 
-    for (i = 0; i < countries_stat.length; i++) {
-        console.log('countries ', countries_stat[i]);
+//--------  GEGEVENS AANMAKEN ---------//
+request.onload = function () {
+    console.log(request.response.countries_stat);
+
+    //hulp van Vincent
+    var getData = request.response.countries_stat; // gewenste data gegevens ophalen
+
+    for (i = 0; i < getData.length; i++) {
+        console.log('country ', getData[i]);
         var option = document.createElement('option'); //option element aanmaken 
-        option.innerHTML = countries_stat[i].country_name; //data landnamen ophalen 
+        option.innerHTML = getData[i].country_name; //data landnamen ophalen 
         //innerHTML takes the existing DOM content of the parent node and replaces it 
         countryList.appendChild(option); //toevoegen van alle landnamen aan select in html
-    };
-    //}
-    //
-    //function infoSelected(onload.dataCheck) {
-    //    //    forE(function (country) {
-    //    if ((country.country_name === selectCountry.value) {
-    //        console.log(country);
-    //
-    //        var totalCases = document.createElement('h1');
-    //        var deaths = document.createElement('h2');
-    //        var sick = document.createElement('h2');
-    //        var recovered = document.createElement('h2');
-    //
-    //        totalCases.textContent = 'Totaal: ' + [country.cases];
-    //        deaths.textContent = 'Doden: ' + [country.deaths];
-    //        sick.textContent = 'Zieken: ' + [country.active_cases];
-    //        recovered.textContent = 'Genezen: ' + [country.total_recovered];
-    //
-    //        countryInfo.appendChild(totalCases);
-    //        countryInfo.appendChild(deaths);
-    //        countryInfo.appendChild(sick);
-    //        countryInfo.appendChild(recovered);
-    //    }, true)}
+    }
+};
 
-
-    //selectie van een land uit de landenlijst met change
-//    selectCountry.addEventListener('change', infoSelected);
-
+   //hulp van Vincent
+selectCountry.addEventListener('change', function (event) {
+    console.log(event);
+    var getData = request.response.countries_stat; // gewenste data gegevens ophalen
     //forEach = excute a function on each element in the array
-    //    data.forEach(function (country) {
-    //        if (country.country_name === selectCountry.value) {
-    //            console.log(country);
-    //        }
+    getData.forEach(function (country) {
+            if (country.country_name === selectCountry.value) {
+                console.log(country);
 
-    //    for (i = 0; i < allData.length; i++) {
-    //
-    //    function country(country.country_name === [i].value)
-    //    console.log(country); }
-    //}
+                var totalCases = document.createElement('h1');
+                var deaths = document.createElement('h2');
+                var sick = document.createElement('h2');
+                var recovered = document.createElement('h2');
+
+                totalCases.textContent = 'Totaal: ' + [country.cases];
+                deaths.textContent = 'Doden: ' + [country.deaths];
+                sick.textContent = 'Zieken: ' + [country.active_cases];
+                recovered.textContent = 'Genezen: ' + [country.total_recovered];
+
+                countryInfo.appendChild(totalCases);
+                countryInfo.appendChild(deaths);
+                countryInfo.appendChild(sick);
+                countryInfo.appendChild(recovered);
+            }
+//kwam hier niet meer uit 
+            if (country.country_name === selectCountry.value > 1) {
+                countryInfo[i++].remove;
+            }
+        }
+
+
+
+        , true);
+})
